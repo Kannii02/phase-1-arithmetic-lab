4a573d00946a91ed7a29183d6e08dc7fd52aca76
kannikarsriburin <i_m_juy-juys@hotmail.co.th>
2025-01-03 11:20:29 -1000

    Complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2; // => 62
+const random = Math.floor(Math.random() * 100) + 1; // A random integer > 0
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4; // => 4
+const max = Math.max(1, 5, 10, 20); // => 20
